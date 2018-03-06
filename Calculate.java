fd95b49fc4e7bca5529083c9d0d3db25593d61e4
Sergei Shangareev <ssr@Sergeis-MBP.Dlink>
2018-03-06 11:37:25 +0500

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,17 @@
 public class Calculate{
 	public static void main (String[] arg){
 			System.out.println("Calculate...");
-			
+			double first = Double.valueOf(arg[0]);
+			double second = Double.valueOf(arg[1]);
+			double summ = first + second;
+
+			double pow = Math.pow(first, second);
+			double minus = second - first;
+
+			System.out.println("Summa " + summ);
+
+			System.out.println("Stepen' " + pow);
+
+			System.out.println("Vichitanie " + minus);
 	}
 }
